c938abd529f94d413a703dc774d23bb8dbfca63a
arpit <arpit2003y@gmail.com>
2024-07-27 13:08:11 +0530

    feat(conf):added_middleware_config

---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
+
+import initDb from "@/models/server/db"
+import getOrCreateStorage from "@/models/server/storageSetup"
+
+export async function middleware(request: NextRequest) {
+    await Promise.all([initDb(), getOrCreateStorage()])
+
+    return NextResponse.next()
+}
+
+export const config = {
+    /* match all paths except : 
+
+	 - api
+	 - _next/static
+	 - _next/image
+	 _ favicon.com
+
+	*/
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+}
