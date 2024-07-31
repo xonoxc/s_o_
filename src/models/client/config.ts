1338b93e5ad421135e175aa965afb28fa8fcda5c
arpit <arpit2003y@gmail.com>
2024-07-31 12:02:20 +0530

    fix(env_client_access):fixed_client_env_load_issue

--- src/models/client/config.ts
+++ src/models/client/config.ts
@@ -3,8 +3,8 @@ import { env } from "@/validation/env"
 import { Client, Avatars, Account, Databases, Storage } from "appwrite"
 
 const client = new Client()
-    .setEndpoint(env.NEXT_PUBLIC_APPWRITE_HOST_URL)
-    .setProject(env.NEXT_PUBLIC_APPWRITE_PROJECT_ID)
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_HOST_URL!)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!)
 
 /* Services initialization */
 const databases = new Databases(client)
--- src/validation/env.ts
+++ src/validation/env.ts
@@ -1,6 +1,6 @@
 import { z } from "zod"
 
-export const envSchema = z.object({
+const envSchema = z.object({
     NEXT_PUBLIC_APPWRITE_HOST_URL: z.string(),
     NEXT_PUBLIC_APPWRITE_PROJECT_ID: z.string(),
     APPWRITE_API_KEY: z.string(),
@@ -10,7 +10,6 @@ const result = envSchema.safeParse(process.env)
 
 if (!result.success) {
     console.error("error validating env variables", result.error)
-    process.exit(1)
 }
 
 export const env = result.data
